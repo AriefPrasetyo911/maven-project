0f716ec362c0e5df0be43d5fd61e6ef7232e484b
Arief Budi Prasetyo <arief@kaigangames.com>
2018-11-28 14:53:06 +0700

    fix error

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
   }
 
   /**
-   * @param someone is name of person.
+   * @param someone is name of person
    * @return greeting string 
    */
   public final String greet(final String someone) {
